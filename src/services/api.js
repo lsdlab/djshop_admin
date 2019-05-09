55d89378210d8942b1c155d1986644e6cd04b35b
Chen Jian <lsdlab@icloud.com>
2019-05-09 13:25:56 +0800

    update

--- src/services/api.js
+++ src/services/api.js
@@ -129,9 +129,10 @@ export async function getFakeCaptcha(mobile) {
 }
 
 
-// const apiHost = 'http://localhost:9000';    // development
-const apiHost = 'https://shopapi.nuclearst.com';    // production
+const apiHost = 'http://localhost:9000';    // development
+// const apiHost = 'https://shopapi.nuclearst.com';    // production
 // const apiHost = 'https://shopapi.lianzongai.com';    // production
+// const apiHost = 'https://shopapi.mldit.com';    // production
 const apiVersion = '/api/v1';
 
 
--- updatelz.sh
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -m "update"
-git push origin master
-git push gitlab master
-npm run build
-scp -r dist root@123.206.178.152:/root/lz/djshop_admin
-echo "update finish"
