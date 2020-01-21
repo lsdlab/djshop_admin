efab7c06b105c94b0dccb436bf0062c63fa37329
Chen Jian <lsdlab@icloud.com>
2020-01-21 16:32:22 +0800

    update

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,24 +1,30 @@
+var apiHost;
+if (process.env.DEPLOY === 'PRODUCTION') {
+  apiHost = 'https://shopapi.mldit.com';
+} else {
+  apiHost = 'http://localhost:9000';
+}
+
 module.exports = {
-  "navTheme": "light",
-  "primaryColor": "#1890FF",
-  "layout": "topmenu",
-  "contentWidth": "Fixed",
-  "fixedHeader": false,
-  "autoHideHeader": false,
-  "fixSiderbar": false,
-  "menu": {
-    "disableLocal": false
+  navTheme: 'light',
+  primaryColor: '#1890FF',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  menu: {
+    disableLocal: false,
   },
-  "title": "严选电商",
-  "pwa": true,
-  "iconfontUrl": "",
-  "collapse": true,
-  "merchantID": "2",
-  // "apiHost": "http://localhost:9000",
-  "apiHost": "https://shopapi.mldit.com",
-  "access_key_id": 'TFRBSWNQcm41WjZDWFl4Qw==',
-  "access_key_secret": 'Wkp4WjJ5RUJnRHNhNEJjcHVMRWxwVG9HejlhS1FV',
-  "region": 'oss-cn-shanghai',
-  "OSS_BUCKET": 'ZGpzaG9wbWVkaWE=',
-  "OSS_ENDPOINT": 'aHR0cHM6Ly9vc3MtY24tc2hhbmdoYWkuYWxpeXVuY3MuY29t',
+  title: '严选电商',
+  pwa: true,
+  iconfontUrl: '',
+  collapse: true,
+  merchantID: '1',
+  apiHost: apiHost,
+  access_key_id: 'TFRBSWNQcm41WjZDWFl4Qw==',
+  access_key_secret: 'Wkp4WjJ5RUJnRHNhNEJjcHVMRWxwVG9HejlhS1FV',
+  region: 'oss-cn-shanghai',
+  OSS_BUCKET: 'ZGpzaG9wbWVkaWE=',
+  OSS_ENDPOINT: 'aHR0cHM6Ly9vc3MtY24tc2hhbmdoYWkuYWxpeXVuY3MuY29t',
 };
